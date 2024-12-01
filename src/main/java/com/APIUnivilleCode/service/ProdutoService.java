47b651756572b5e0d53c0d32de97420369ba731e
jeffersonsantos30 <jeferson29santos@gmail.com>
2024-12-01 17:58:17 -0300

    01122024

--- src/main/java/com/APIUnivilleCode/service/ProdutoService.java
+++ src/main/java/com/APIUnivilleCode/service/ProdutoService.java
@@ -2,7 +2,6 @@ package com.APIUnivilleCode.service;
 
 import java.util.List;
 
-import org.apache.el.stream.Optional;
 import org.springframework.stereotype.Service;
 
 import com.APIUnivilleCode.model.Produto;
